e0b9c747a59f777404007c6f0718b3c639b170b5
Dharmik <dharmikpajwanigithub@gmail.com>
2021-01-07 20:50:21 +0530

    Custom css file added and css files linked to index.php

--- index.php
+++ index.php
@@ -3,6 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap CSS -->
+    <link rel = "stylesheet" href = "css/bootstrap.min.css">
+
+    <!-- FontAwesome CSS -->
+    <link rel = "stylesheet" href = "css/all.min.css">
     <title>theIndus</title>
 </head>
 <body>
